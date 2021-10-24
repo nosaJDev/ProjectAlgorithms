9bd4c7a1fa5d449b9dc09d35250d73cf77dafe18
Loukas Bozikis <jason.dev.42@gmail.com>
2021-10-24 18:05:16 +0300

    Created function to automatically include close hamming distance numbers

--- dstructs/hcubemap.cpp
+++ dstructs/hcubemap.cpp
@@ -1,5 +1,86 @@
 #include "hcubemap.hpp"
 
+int * inclusiveHamming(int number, int bits, int dist, HashTable * ht = nullptr){
+
+    // This function takes a number, and the number of viable bits,
+    // and returns an array of numbers with less than or equal hamming
+    // distance to it.
+    // This is done through recursion
+    // The elements are saved in a hashtable until returning to the first call
+
+
+    //If the hashtable is a nullptr, this signifies the first call
+    bool firstcall = ht == nullptr;
+    if(firstcall){
+        // Create the hash table
+        ht = new HashTable(50);
+    }
+
+    if (dist <= 0){
+        // If the distance is zero, only this number applies
+        // Insert it to the hash table
+        ht->add(nullptr,number);
+
+    }else{
+        // If the distance is more, find all the numbers with distance one,
+        // and recursively fill the other distances
+        
+        // Find the numbers with distance == 1, by xoring and bitshifts
+        int at = 1;
+        for(int i = 0; i < bits; i++){
+
+            // Find the number
+            int num = number ^ at;
+
+            // Insert it at the hash table
+            ht->add(nullptr,num);
+
+            // Recursively call the function for lower distances
+            inclusiveHamming(num,bits,dist-1,ht);
+
+            // Increment the at variable
+            at = at << 1;
+        }
+
+        // Finally add the initial number to the hash table
+        ht->add(nullptr,number);
+
+    }
+
+    if(firstcall){
+        // If this is the first call, find all the elements on the table, and
+        // return them in the form of the array stated above
+
+        // Create the array to hold the elements
+        int * ret = new int[ht->getElems()+1];
+
+        // Find the contents of the hash table and add them to the array
+        ret[0] = ht->getElems(); // This represents the number of elements
+        int at = 1;
+        List ** buckets = ht->getAllChains();
+        for(int i = 0; i < ht->getSize(); i++){
+            List * bucket = buckets[i];
+            for(int j = 0; j < bucket->getElems(); j++){
+                HashElement * he = (HashElement*)bucket->get(j);
+                ret[at++] = he->key;
+            }
+        }
+
+        // Delete the hash table and return the result
+        delete ht;
+        return ret;
+
+
+    }else{
+        // If it's not, the return value doesn't matter
+        return nullptr;
+
+    }
+
+
+}
+
+
 HypercubeMapping::HypercubeMapping(int cdims, int _w, int dims){
 
     // This will initiate all the parameters and the function generator
